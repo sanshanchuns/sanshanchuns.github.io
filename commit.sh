ffdf81e2e0b4f2850759556fec0b10366f8cd450
leozhu <zhuli2@noahwm.com>
2015-12-01 11:54:49 +0800

    update files

--- commit.sh
+++ commit.sh
@@ -8,11 +8,13 @@ current_branch=`git rev-parse --abbrev-ref HEAD`
 if [ -z "$1" ];then
 	git add .
 	git ci -m"update files"
+	git pull --rebase $remote1 $current_branch
 	git push $remote1 $current_branch
-#	git push $remote2 $current_branch
+	# git push $remote2 $current_branch
 else
 	git add .
 	git ci -m"$1"
+	git pull --rebase $remote1 $current_branch
 	git push $remote1 $current_branch
-#	git push $remote2 $current_branch
+	# git push $remote2 $current_branch
 fi
